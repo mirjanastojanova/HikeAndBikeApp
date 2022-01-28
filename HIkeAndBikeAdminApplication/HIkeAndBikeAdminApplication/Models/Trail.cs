7a8e6c5243ecc1a26fad8d2abdc5daef768f9799
Mirjana Stojanova <Mirjana Stojanova>
2022-01-28 22:03:45 +0100

    aud done

---
+++ HIkeAndBikeAdminApplication/HIkeAndBikeAdminApplication/Models/Trail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HIkeAndBikeAdminApplication.Models
+{
+    public class Trail
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public Double Price { get; set; }
+        public DateTime Date { get; set; }
+        public string Guide { get; set; }
+    }
+}
---
+++ HIkeAndBikeAdminApplication/HIkeAndBikeAdminApplication/Models/TrailInOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HIkeAndBikeAdminApplication.Models
+{
+    public class TrailInOrder
+    {
+        public Guid TrailId { get; set; }
+        public Trail SelectedTrail { get; set; }
+        public Guid OrderId { get; set; }
+        public Order UserOrder { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
